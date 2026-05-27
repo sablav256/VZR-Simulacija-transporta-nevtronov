2b87a97aa544baf66bb8bceb31c2606b9e7e9327
sablav256 <152267025+sablav256@users.noreply.github.com>
2026-05-27 18:32:44 +0200

    Implement CSV saving for simulation results
    Added functionality to save simulation results to a CSV file.

--- numba_accel.py
+++ numba_accel.py
@@ -1,84 +1,69 @@
 import numpy as np
 from numba import njit
 import time
+import csv
+import os
 
 
-# Numba - optimizirana Monte Carlo simulacija transporta nevtronov
-# Simulacija nevtronov znotraj enega procesa
 @njit
 def simulate_batch(n, thickness, mean_free_path, absorption_prob):
 
-    # začetne vrednosti za opazovanje izida nevtronov
     absorbed = 0
     transmitted = 0
     reflected = 0
 
-    # simulacija posameznih nevtronov
     for _ in range(n):
 
-        # začetni položaj nevtrona
         x = 0.0
-
-        # začetna smer gibanja (+x smer)
         angle = 0.0
 
         while True:
 
-            # generiranje proste poti
-            # eksponentna porazdelitev
             step = -mean_free_path * np.log(np.random.random())
-
-            # premik nevtrona v trenutni smeri
             x += step * np.cos(angle)
 
-            # TRANSMISIJA
-            # nevtron zapusti material
             if x >= thickness:
                 transmitted += 1
                 break
 
-            # REFLEKSIJA
-            # nevtron se odbije nazaj
             if x <= 0:
                 reflected += 1
                 break
 
-            # ABSORPCIJA
-            # material absorbira nevtron
             if np.random.random() < absorption_prob:
                 absorbed += 1
                 break
 
-            # SIPANJE
-            # nova naključna smer gibanja
             angle = -np.pi + 2 * np.pi * np.random.random()
 
     return absorbed, transmitted, reflected
 
 
+def save_result(method, cores, t):
+    file_exists = os.path.isfile("times.csv")
+
+    with open("times.csv", "a", newline="") as f:
+        writer = csv.writer(f)
+        if not file_exists:
+            writer.writerow(["method", "cores", "time"])
+
+        writer.writerow([method, cores, t])
+
+
 if __name__ == "__main__":
-
-    # parametri materiala
-    # debelina materiala
-    thickness = 10.0
-    # povprečna prosta pot nevtrona (razdalja med trki)
-    mean_free_path = 1.0
-    # verjetnost absorpcije pri posameznem trku
-    absorption_prob = 0.25
-
-    # število simuliranih nevtronov
-    n = 1000000
 
     start = time.time()
 
     result = simulate_batch(
-        n,
-        thickness,
-        mean_free_path,
-        absorption_prob
+        200000,
+        10.0,
+        1.0,
+        0.25
     )
 
     end = time.time()
 
     print("RESULT:", result)
     print("TIME:", end - start)
+
+    save_result("numba", 1, end - start)
